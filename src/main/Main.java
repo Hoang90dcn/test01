a80f2674b64bd84095b5e29f9e3a8946a80b8401
Hoang90dcn <huyhoang100599@gmail.com>
2020-08-02 17:08:32 +0700

    thu lan 2

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.math.BigDecimal;
+
+public class Main {
+	public static void main(String[] args) {
+		BigDecimal total;
+		int i =9;
+		BigDecimal tem = new BigDecimal(i);
+		double t = 4.876;
+		System.out.println(t);
+		total = new BigDecimal(t);
+		System.out.println(t*i);
+		
+	}
+}
